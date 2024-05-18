ad3d73b13f18c06e5b92b9bc5e2c966f83dad07a
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-05-18 21:19:22 +0100

    fixed reload button height

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
                 className=" -mt-16 md:mt-0 min-w-16 md:min-h-16  md:absolute p-2 md:center  md:top-2 md:right-0 rounded-full text-center   bg-purple-600 md:mr-16"
               >
     
-                <img src="./src/assets/icon-arrow.svg" alt="" />
+                <img src="../src/assets/icon-arrow.svg" alt="" />
               </button>
             </div>
           </form>
