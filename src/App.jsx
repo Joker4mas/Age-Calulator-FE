88943533ace5784c3386ab266095aa815ee486fa
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-05-18 18:19:10 +0100

    Fixed the mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   differenceInMonths,
   differenceInYears,
 } from "date-fns";
-
+// import Icons from './assets/icon-arrow.svg'
 import Footer from './components/footer.jsx'
 
 // import {Arrow} from './assets/icon-arrow.sv'
@@ -29,16 +29,16 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center my-8">
+      <div className="flex flex-col justify-center items-center my-4">
         <div
           style={{ borderBottomRightRadius: 100 }}
-          className="   bg-white  rounded-2xl text-center items-center p-8"
+          className=" w-half bg-white  rounded-2xl text-center p-4 items-center lg:p-8"
         >
-          <h1 className="text-3xl font-bold">Age Calculator</h1>
+          <h1 className="hidden md:text-3xl font-bold">Age Calculator</h1>
 
           <form id="form">
-            <div className="container  flex flex-row gap-4 lg:gap-12 justify-center my-8 mx-auto">
-              <div className="flex flex-col gap-2 items-start">
+            <div className="container  flex flex-row gap-8 md:gap-4 lg:gap-12 justify-center my-8 mx-auto">
+              <div className="flex flex-col gap-1 items-start">
                 {" "}
                 <label htmlFor="number" className={`${days > 31 && "text-rose-600",'font-bold'}`}>DAY</label>
                 <input
@@ -48,14 +48,14 @@ function App() {
                   placeholder="DD"
                   min="1"
                   max="31"
-                  className="sm:w-14 md:w-28 p-2 text-3xl font-bold rounded-md border-solid border-2 border-slate-400"
+                  className="w-16 lg:w-28 p-2  sm:text-xl md:text-3xl font-bold rounded-md border-solid border-2 border-slate-400"
                   value={days}
                   onChange={(e) => setDay(e.target.value)}
                 />
-                {days > 31 && (<small className="text-rose-600">Must be a valid day</small>)}
+                {days > 31 && (<small className="block text-rose-600">Invalid days</small>)}
               </div>
 
-              <div className="flex flex-col gap-2 items-start">
+              <div className="flex flex-col gap-1 items-start">
                 <label htmlFor="month" className={`${months > 12 && 'text-rose-600','font-bold'}`}>MONTH</label>
                 <input
                   type="number"
@@ -63,63 +63,64 @@ function App() {
                   placeholder="MM"
                   min="1"
                   max="12"
-                  className="sm:w-14 lg:w-28 text-3xl p-2 font-bold  rounded-md border-solid border-2 border-slate-400"
+                  className="w-16 lg:w-28 sm:text-xl md:text-3xl p-2 font-bold  rounded-md border-solid border-2 border-slate-400"
                   value={months}
                   onChange={(e) => setMonth(e.target.value)}
                 />
-                {months > 12 && (<small className="text-rose-600">Must be a valid Month</small>)}
+                {months > 12 && (<small className="text-rose-600">Invalid Month</small>)}
 
               </div>
 
-              <div className="flex flex-col gap-2 items-start">
+              <div className="flex flex-col gap-1 items-start">
                 <label htmlFor="year" className={`${years > 2050 && 'text-rose-600','font-bold'}`}>YEAR</label>
                 <input
                   type="Year"
                   name="number"
                   id="year"
                   placeholder="YYYY"
-                  className=" w-28 text-3xl p-2 font-bold rounded-md  border-solid border-2 border-slate-400"
+                  className="w-16 lg:w-28 lg:text-3xl p-2 font-bold rounded-md  border-solid border-2 border-slate-400"
                   value={years}
                   onChange={(e) => setYear(e.target.value)}
                 />
-                {years > 2050 && (<small className="text-rose-600">Must be a valid Year</small>)}
+                {years > 2050 && (<small className="text-rose-600">Invalid Year</small>)}
               </div>
             </div>
-            <div className=" relative items-center text-center px-4 py-1  ">
+            <div className="block lg:relative items-center text-center px-4 py-1  ">
 
-                <div>
-              <hr className="border-solid border-1 border-slate-300 my-8 w-96 ml-24" />
+                <div className="text-center mx-auto">
+              <hr className="w-72 lg:w-96 border-solid border-1 border-slate-300 my-8 lg:ml-24" />
                 </div>            
                 <button
                 type="submit"
-                className="absolute p-2 top-2 right-0 rounded-full text-center   bg-purple-600 mr-16"
+                className=" -mt-16 md:mt-0  md:absolute p-2 md:center  md:top-2 md:right-0 rounded-full text-center   bg-purple-600 md:mr-16"
               >
-                <img src="/src/assets/icon-arrow.svg" alt="" />
+    
+                <img src="./src/assets/icon-arrow.svg" alt="" />
               </button>
             </div>
           </form>
 
           {/* The logics displays here  */}
-          <div className="flex flex-col justify-center ml-4 items-start text-center ">
+          <div className="flex flex-col justify-center md:ml-4 mb-4 items-start text-center ">
             <div className="flex flex-row gap-6 font-bold italic">
-              <span className="font-bold text-8xl text-purple-500">
+              <span className="font-bold text-7xl lg:text-8xl text-purple-500">
                 {diffInYears}
               </span>{" "}
-              <span className="font-bold text-8xl">Years</span>
+              <span className="font-bold text-7xl lg:text-8xl">Years</span>
             </div>
 
             <div className="flex flex-row gap-6 italic">
-              <span className="font-bold text-8xl text-purple-500">
+              <span className="font-bold text-7xl lg:text-8xl text-purple-500">
                 {diffInMonths}
               </span>
-              <span className="font-bold text-8xl ">Months</span>
+              <span className="font-bold text-7xl lg:text-8xl ">Months</span>
             </div>
 
             <div className="flex flex-inline gap-6 italic">
-              <span className="font-bold text-8xl text-purple-500">
+              <span className="font-bold text-7xl lg:text-8xl text-purple-500">
                 {daysDiff}
               </span>
-              <span className="font-bold text-8xl">Days</span>
+              <span className="font-bold text-7xl lg:text-8xl">Days</span>
             </div>
           </div>
         </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <div>
-            <div className='flex text-white text-2xl my-4'>
+            <div className='flex text-white text-center text-2xl my-6'>
                 <p>Designed by Joker4mas 2024 @Frontend Mentors Challenge</p>
             </div>
         </div>
